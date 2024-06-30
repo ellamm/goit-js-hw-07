20dc49f109192a32dc5316fb5148bfa4505847f8
Mihaela <26812755+ellamm@users.noreply.github.com>
2024-06-30 12:17:34 +0300

    task 1 and 2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,44 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryRef = document.querySelector('.gallery');
+
+//Gallery Items
+const galleryItemsMarkup = createGalleryItemsMarkUp(galleryItems);
+
+galleryRef.innerHTML = galleryItemsMarkup;
+
+function createGalleryItemsMarkUp(items) {
+  return items.map(({ preview, original, description}) => `<div class="gallery__item">
+  <a class="gallery__link" href=${original}>
+    <img
+      class="gallery__image"
+      src=${preview}
+      data-source=${original}
+      alt=${description}
+    />
+  </a>
+</div>`).join('');
+}
+
+// Open/close original image
+galleryRef.addEventListener('click', onGalleryItemClick);
+
+function onGalleryItemClick(e) {
+  e.preventDefault();
+  const originalImgUrl = e.target.dataset.source;
+
+  const lightBoxModal = basicLightbox.create(
+    `<img src= ${originalImgUrl} width="800" height="600">`, {
+    onClose: () => document.removeEventListener("keydown", onEscapeKey)
+  });
+  lightBoxModal.show();
+
+  // Keypad to close the opened image
+  document.addEventListener("keydown", onEscapeKey);
+  function onEscapeKey(e) {
+    if (e.code === 'Escape') {
+      lightBoxModal.close();
+    }
+  };
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,24 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryRef = document.querySelector('.gallery');
+
+//Gallery Items Markup
+const galleryItemsMarkup = createGalleryItemsMarkUp(galleryItems);
+
+galleryRef.innerHTML = galleryItemsMarkup;
+
+function createGalleryItemsMarkUp(items) {
+  return items.map(({ preview, original, description }) =>
+    `<a class="gallery__item" href=${original}>
+  <img class="gallery__image" src=${preview} alt=${description} />
+</a>`).join('');
+}
+
+// image processing by Lightbox library
+const gallery = new SimpleLightbox('.gallery a', {
+    captions: true,
+    captionsData: "alt",
+  captionDelay: 250,
+});
+gallery.on('show.simplelightbox', function () {});
